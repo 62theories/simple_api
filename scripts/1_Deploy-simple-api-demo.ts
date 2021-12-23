6b3e9e0c63816231eabd89ad371b57af782a865d
62theories <s5935512005@phuket.psu.ac.th>
2021-12-23 14:00:46 +0700

    change type to any ( type is gitignore )

--- scripts/1_Deploy-simple-api-demo.ts
+++ scripts/1_Deploy-simple-api-demo.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
 import { ethers, artifacts } from "hardhat";
-import { ApiDemo } from "../typechain/ApiDemo";
+// import { ApiDemo } from "../typechain/ApiDemo";
 
 async function main() {
   const contract = await ethers.getContractFactory("ApiDemo");
@@ -13,14 +13,17 @@ async function main() {
   saveContract(api);
 }
 
-function saveContract(api: ApiDemo) {
-  const path = __dirname + '/../frontend/src/contracts';
-  if (!fs.existsSync(path))
-    fs.mkdirSync(path);
-  fs.writeFileSync(`${path}/address.json`,
-    JSON.stringify({ address: api.address }, undefined, 2))
-  fs.writeFileSync(`${path}/abi.json`,
-    JSON.stringify(artifacts.readArtifactSync('ApiDemo'), undefined, 2))
+function saveContract(api: any) {
+  const path = __dirname + "/../frontend/src/contracts";
+  if (!fs.existsSync(path)) fs.mkdirSync(path);
+  fs.writeFileSync(
+    `${path}/address.json`,
+    JSON.stringify({ address: api.address }, undefined, 2)
+  );
+  fs.writeFileSync(
+    `${path}/abi.json`,
+    JSON.stringify(artifacts.readArtifactSync("ApiDemo"), undefined, 2)
+  );
 }
 
 main().catch((error) => {
